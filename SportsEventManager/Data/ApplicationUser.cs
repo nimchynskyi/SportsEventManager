1778b38555adf20c7b04ed09398269e2ebf7a753
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2024-08-28 20:58:34 -0600

    Add User roles

--- SportsEventManager/Data/ApplicationUser.cs
+++ SportsEventManager/Data/ApplicationUser.cs
@@ -6,8 +6,11 @@ namespace SportsEventManager.Data;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
-    public DateOnly? DateOfBirth { get; set; }
-
     [StringLength(50)] public string? FirstName { get; set; }
     [StringLength(50)] public string? LastName { get; set; }
+    public DateOnly? DateOfBirth { get; set; }
+    
+    public bool IsOrganizer { get; set; }
+    public bool IsPlayer { get; set; }
+    public bool IsCoach { get; set; }
 }
---
+++ SportsEventManager/Migrations/20240829025656_UserRoles.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsEventManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCoach",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOrganizer",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPlayer",
+                table: "AspNetUsers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCoach",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsOrganizer",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsPlayer",
+                table: "AspNetUsers");
+        }
+    }
+}
